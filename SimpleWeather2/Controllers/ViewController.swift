4f5c141a21eb55b79a351110e1818ba16af49e10
jessicakohh <juko95@me.com>
2022-10-06 19:15:19 +0900

    Add VC

--- SimpleWeather2/Controllers/ViewController.swift
+++ SimpleWeather2/Controllers/ViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 class ViewController: UIViewController {
-
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        self.locationManager.requestWhenInUseAuthorization()
 
+
+    }
 
 }
 
+
+//MARK: - CLLocationManagerDelegate
+
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            //            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
